876cfc82e11a9343c923c625e79e52e2a3d5ca17
vit9696 <vit9696@users.noreply.github.com>
2017-09-27 21:31:04 +0300

    Added initial crypto and nvram API implementions (thx to lvs1974 for correcting them)

--- KnownPlugins.md
+++ KnownPlugins.md
@@ -4,22 +4,23 @@ Existing Lilu plugins
 This for sure may not be a complete list. If you feel that something is missing, make an issue and it will be included.  
 Please note that improperly written immature plugins will not be listed there.
 
-| Name | Short description |
-|:-----|:------------------|
-[AirportBrcmFixup](https://sourceforge.net/p/airportbrcmfixup) | Various patches for Broadcom Airport Wi-Fi cards
-[AppleALC](https://github.com/vit9696/AppleALC) | Native macOS HD audio for not officially supported codecs
-[AzulPatcher4600](https://github.com/coderobe/AzulPatcher4600) | Additional Intel HD 4600 patches for some laptops on 10.12.4+
-[BT4LEContiunityFixup](https://sourceforge.net/p/bt4lecontiunityfixup) | Enable BT4LE-Handoff-Hotspot features
-[CoreDisplayFixup](https://github.com/PMheart/CoreDisplayFixup) | Non Iris Intel graphics high resolution support
-[CPUFriend](https://github.com/PMheart/CPUFriend) | Dynamic power management data injection
-[EnableLidWake](https://github.com/syscl/EnableLidWake) | Enable lid wake for Intel Graphics Card
-[HibernationFixup](https://sourceforge.net/p/hibernationfixup) | Enable 3 & 25 mode hibernation on certain hardware
-[IntelGraphicsFixup](https://sourceforge.net/p/intelgraphicsfixup) | Various patches necessary for certain Intel GPUs
-[IntelGraphicsDVMTFixup](https://github.com/BarbaraPalvin/IntelGraphicsDVMTFixup) | Patches for DVMT problem on Broadwell/Skylake Intel Graphics
-[NightShiftUnlocker](https://github.com/Austere-J/NightShiftUnlocker) | Enables Night Shift on all the models
-[NvidiaGraphicsFixup](https://sourceforge.net/p/nvidiagraphicsfixup) | Various patches necessary for certain NVIDIA GPUs
-[Shiki](https://github.com/vit9696/Shiki) | Dynamic userspace patcher for macOS
-[WhateverGreen](https://github.com/vit9696/WhateverGreen) | Various patches necessary for certain ATI/AMD GPUs
+| Name | Short description | Lilu 1.2.0 compatibility |
+|:-----|:------------------|:----|
+[AirportBrcmFixup](https://sourceforge.net/p/airportbrcmfixup) | Various patches for Broadcom Airport Wi-Fi cards | Source code
+[AppleALC](https://github.com/vit9696/AppleALC) | Native macOS HD audio for not officially supported codecs | Source code
+[ATH9KFixup](https://github.com/chunnann/ATH9KFixup) | Various patches for unsupported Atheros Wi-Fi cards | NO
+[AzulPatcher4600](https://github.com/coderobe/AzulPatcher4600) | Additional Intel HD 4600 patches for some laptops on 10.12.4+ | NO
+[BT4LEContiunityFixup](https://sourceforge.net/p/bt4lecontiunityfixup) | Enable BT4LE-Handoff-Hotspot features | Source code
+[CoreDisplayFixup](https://github.com/PMheart/CoreDisplayFixup) | Non Iris Intel graphics high resolution support | Source code
+[CPUFriend](https://github.com/PMheart/CPUFriend) | Dynamic power management data injection | Source code
+[EnableLidWake](https://github.com/syscl/EnableLidWake) | Enable lid wake for Intel Graphics Card | NO
+[HibernationFixup](https://sourceforge.net/p/hibernationfixup) | Enable 3 & 25 mode hibernation on certain hardware | Source code
+[IntelGraphicsFixup](https://sourceforge.net/p/intelgraphicsfixup) | Various patches necessary for certain Intel GPUs | Source code
+[IntelGraphicsDVMTFixup](https://github.com/BarbaraPalvin/IntelGraphicsDVMTFixup) | Patches for DVMT problem on Broadwell/Skylake Intel Graphics | NO
+[NightShiftUnlocker](https://github.com/Austere-J/NightShiftUnlocker) | Enables Night Shift on all the models | NO
+[NvidiaGraphicsFixup](https://sourceforge.net/p/nvidiagraphicsfixup) | Various patches necessary for certain NVIDIA GPUs | Source code
+[Shiki](https://github.com/vit9696/Shiki) | Dynamic userspace patcher for macOS | Source code
+[WhateverGreen](https://github.com/vit9696/WhateverGreen) | Various patches necessary for certain ATI/AMD GPUs | Source code
 
 Plugins which functionality was merged into other plugins:
 
---
+++ Lilu/Headers/kern_nvram.hpp
@@ -0,0 +1,153 @@
+//
+//  kern_nvram.hpp
+//  Lilu
+//
+//  Copyright © 2017 vit9696. All rights reserved.
+//
+
+#ifndef kern_nvram_hpp
+#define kern_nvram_hpp
+
+#include <Library/LegacyIOService.h>
+#include <libkern/c++/OSSymbol.h>
+#include <stddef.h>
+#include <stdint.h>
+
+class NVStorage {
+	/**
+	 *  Local nvram controller reference
+	 */
+	IORegistryEntry *dtEntry {nullptr};
+	
+public:
+	/**
+	 *  Compress data with a default compression algorithm
+	 *
+	 *  @param src        source data
+	 *  @param size       data size (updated with new size)
+	 *  @param sensitive  contains sensitive data
+	 *
+	 *  @return compressed data (must be freed with Buffer::deleter) or nullptr
+	 */
+	EXPORT uint8_t *compress(const uint8_t *src, uint32_t &size, bool sensitive=false);
+	
+	/**
+	 *  Decompress data compressed with compress
+	 *
+	 *  @param src        compressed data
+	 *  @param size       data size (updated with new size)
+	 *  @param sensitive  contains sensitive data
+	 *
+	 *  @return decompressed data (must be freed with Buffer::deleter) or nullptr
+	 */
+	EXPORT uint8_t *decompress(const uint8_t *src, uint32_t &size, bool sensitive=false);
+	
+public:
+	/**
+	 *  Value storage options
+	 */
+	enum Options {
+		OptAuto         = 0,  // Default options
+		OptRaw          = 1,  // i/o as raw buffer
+		OptCompressed   = 2,  // Apply compression (see kern_compression.hpp)
+		OptEncrypted    = 4,  // Apply encryption with device-unique key (see kern_crypto.hpp)
+		OptChecksum     = 8,  // Append CRC32 checksum to the end
+		OptSensitive    = 16  // Value contains sensitive data
+	};
+	
+	/**
+	 *  Prepended value header unless OptRaw is used
+	 *  After the header the following fields should go:
+	 *  uint8_t iv[16]; aes initialisation vector (if OptEncrypted is set)
+	 *  uint32_t size;  decryption size           (if OptEncrypted is set, encrypted)
+	 *  uint32_t size;  decompression size        (if OptCompressed is set, encrypted if OptEncrypted)
+	 *  uint8_t data[]; content data              (encrypted if OptEncrypted)
+	 *  uint32_t crc32; CRC32 cheksum             (if OptChecksum is set)
+	 */
+	struct PACKED Header {
+		static constexpr uint16_t Magic = 0x9696;
+		static constexpr uint8_t MaxVer = 1;
+		using Checksum = uint32_t;
+		
+		uint16_t magic {Magic};
+		uint8_t version {MaxVer};
+		uint8_t opts;
+	};
+	
+	/**
+	 *  Attempt to connect to active nvram, may fail at early stages
+	 *
+	 *  @return true on success
+	 */
+	EXPORT bool init();
+
+	/**
+	 *  Relinquish resources used, must be called regardless of the init error
+	 */
+	EXPORT void deinit();
+	
+	/**
+	 *  Read data from nvram
+	 *
+	 *  @param key    key name
+	 *  @param size   amount of data read
+	 *  @param opts   bitmask of Options, may set option requirements
+	 *  @param enckey encryption key (platform-defined if OptEncrypted is set)
+	 *
+	 *  @return pointer to data (must be freed via Buffer::deleter), nullptr on failure
+	 */
+	EXPORT uint8_t *read(const char *key, uint32_t &size, uint8_t opts=OptAuto, const uint8_t *enckey=nullptr);
+	
+	/**
+	 *  Write data to nvram
+	 *
+	 *  @param key    key name
+	 *  @param src    source buffer
+	 *  @param size   buffer size
+	 *  @param opts   bitmask of Options
+	 *  @param enckey encryption key (platform-defined if OptEncrypted is set)
+	 *
+	 *  @return true on success
+	 */
+	EXPORT bool write(const char *key, const uint8_t *src, uint32_t sz, uint8_t opts=OptAuto, const uint8_t *enckey=nullptr);
+	
+	/**
+	 *  Delete key from nvram
+	 *
+	 *  @param key        key name
+	 *  @param sensitive  sensitive data
+	 *
+	 *  @return true on successful deletion or if key is missing
+	 */
+	EXPORT bool remove(const char *key, bool sensitive=false);
+	
+	/**
+	 *  Synchronize with nvram controller
+	 *  This method might fail if synchronisation was done recently.
+	 *
+	 *  @return true if synchronised
+	 */
+	EXPORT bool sync();
+	
+	/**
+	 *  Exports nvram to a plist file
+	 *
+	 *  @param filename  file path
+	 *  @oaram max       max output size
+	 *  @param sensitive contains sensitive data
+	 *
+	 *  @return true if saved
+	 */
+	EXPORT bool save(const char *filename, uint32_t max=0x20000, bool sensitive=false);
+
+	/**
+	 *  Check whether key exists
+	 *
+	 *  @param key        key name
+	 *
+	 *  @return true if key exists
+	 */
+	EXPORT bool exists(const char *key);
+};
+
+#endif /* kern_nvram_hpp */
