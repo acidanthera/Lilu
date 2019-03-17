702828ae3dcac7b0f62ce5e1f1006cf826064cdf
vit9696 <vit9696@users.noreply.github.com>
2019-03-17 12:14:27 +0300

    Fix typo

--- Lilu/Headers/kern_iokit.hpp
+++ Lilu/Headers/kern_iokit.hpp
@@ -157,7 +157,7 @@ namespace WIOKit {
 			Ex3DController    = 0x030200,
 			DisplayController = 0x038000,
 			PCIBridge         = 0x060400,
-			// HDA device on some laptops liek Acer Aspire VN7-592G (INSYDE)
+			// HDA device on some laptops like Acer Aspire VN7-592G (INSYDE).
 			HDAMmDevice       = 0x040100,
 			// Watch out for PCISubclassMask, 0x040380 is common on laptops.
 			HDADevice         = 0x040300,
