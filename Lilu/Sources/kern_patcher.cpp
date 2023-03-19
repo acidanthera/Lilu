525b7ab9b42f0c3c1baa207a1bd5bd30244db670
Nyan Cat <17372086+e666666@users.noreply.github.com>
2023-03-19 15:00:49 +0800

    Print orgVmMapEnterMemObjectControl return value

--- Lilu/Sources/kern_patcher.cpp
+++ Lilu/Sources/kern_patcher.cpp
@@ -438,7 +438,7 @@ kern_return_t KernelPatcher::onVmMapEnterMemObjectControl(
 			  (target_map, address, initial_size, mask, flags, vmk_flags, tag,
 			   control, offset, copy, cur_protection, max_protection, inheritance);
 		if (kcType != nullptr) {
-			SYSLOG("patcher", "onVmMapEnterMemObjectControl: Returning address %p", *address);
+			SYSLOG("patcher", "onVmMapEnterMemObjectControl: ret=%d with *address set to %p", ret, *address);
 		}
 	}
 
