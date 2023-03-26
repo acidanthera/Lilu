b2351a69d9d7ff47516050a429f64565b9e21ab3
NyanCatTW1 <17372086+NyanCatTW1@users.noreply.github.com>
2023-03-26 21:35:46 +0800

    Panic upon onVmMapEnterMemObjectControl fail

--- Lilu/Sources/kern_patcher.cpp
+++ Lilu/Sources/kern_patcher.cpp
@@ -468,7 +468,7 @@ kern_return_t KernelPatcher::onVmMapEnterMemObjectControl(
 			  (target_map, address, initial_size, mask, flags, vmk_flags, tag,
 			   control, offset, copy, cur_protection, max_protection, inheritance);
 		if (doOverride) {
-			if (ret) SYSLOG("patcher", "onVmMapEnterMemObjectControl: ret=%d with *address set to %p", ret, *address);
+			PANIC_COND(ret, "mach", "onVmMapEnterMemObjectControl: ret=%d with *address set to %p", ret, *address);
 			uint8_t *patchedKC = that->kcMachInfos[kcType]->getFileBuf();
 			memcpy((void*)*address, patchedKC + realOffset, (size_t)initial_size);
 		}
